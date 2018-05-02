6b2e8a7f391c9d705d1f1200d980a5bcc45358ce
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-05-02 22:02:42 +0200

    Update dnsquery.sh

--- dnsquery.sh
+++ dnsquery.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# http://dev.lab427.net/dns-query-wth-netcat.html
 echo -n -e "\x13\x37\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x13google-public-dns-a\x06google\x03com\x00\x00\x01\x00\x01" | nc -u -w1 8.8.8.8 53 | hexdump -C
